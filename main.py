9bceefdd85702b7911aa3bf8a8f46f26b85a4af9
beerens20 <beerens20@gmail.com>
2025-03-20 19:17:10 +0000

    add framerate lock and circleshape class

--- main.py
+++ main.py
@@ -4,16 +4,17 @@ from constants import *
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
 
+    # start game loop
     while True:
         for event in pygame.event.get():
-            if event.type == pygame.QUIT:
+            if event.type == pygame.QUIT: # quit and close window if X is clicked
                 return
-        screen.fill("black")
-        pygame.display.flip()
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
-    print("Starting Asteroids!")
+        screen.fill("black") # color the screen
+        pygame.display.flip() # render the page
+        dt = clock.tick(60) # set framerate and return delta
 
 
 if __name__ == "__main__":
